b3486ca3f33d1cca26a728fc7ed99579c58a05d9
Dharshini <148347809+Dharshini-080@users.noreply.github.com>
2025-05-12 13:08:42 +0530

    Add files via upload

---
+++ assignment3/ifloop.java
@@ -0,0 +1,12 @@
+class ifloop {
+    public static void main(String args[])
+    {
+        String str = "Dharshini";
+        int i = 4;
+        if (i == 4) {
+            i++;
+            System.out.println(str);
+        }
+        System.out.println("i = " + i);
+    }
+}
---
+++ assignment3/stloop.java
@@ -0,0 +1,11 @@
+class stloop {
+    public static void main(String[] args)
+    {
+        String[] names = { "Dharshini", "Salem", "Shakthi" };
+
+        for (String name : names) 
+		{
+            System.out.println("Name: " + name);
+        }
+    }
+}
