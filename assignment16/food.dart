a537d38c51016162ea374e1888c0e205921e7044
Dharshini <148347809+Dharshini-080@users.noreply.github.com>
2025-05-29 17:07:22 +0530

    Add files via upload

---
+++ assignment16/food.dart
@@ -0,0 +1,45 @@
+class Food {
+  String? name;
+  double? price;
+  String? image;
+
+  Food({this.name, this.price, this.image});
+}
+  List foo = [
+    Food(
+      name: "Biriyani",
+      price: 120.0,
+      image: "assets/images/biriyani.png",
+    ),
+    Food(
+      name: "Pizza",
+      price: 250.0,
+      image: "assets/images/pizza.png",
+    ),
+    Food(
+      name: "Burger",
+      price: 150.0,
+      image: "assets/images/burger.png",
+    ),
+    Food(
+      name: "Pasta",
+      price: 180.0,
+      image: "assets/images/pasta.png",
+    ),
+    Food(
+      name: "Fried Rice",
+      price: 130.0,
+      image: "assets/images/friedrice.png",
+    ),
+    Food(
+      name: "Samosa",
+      price: 20.0,
+      image: "assets/images/samosa.png",
+    ),
+    Food(
+      name: "Parota",
+      price: 15.0,
+      image: "assets/images/parota.png",
+    ),
+  ];
+
